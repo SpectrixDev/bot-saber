20abccb8773f1f07ae81fbd83788dc8872c1be96
Spectrix <owenhutch123@gmail.com>
2020-10-09 15:10:24 +0200

    Updated OneClick install directory on my website
    Changes can be found here https://github.com/SpectrixOfficial/spectrix.pythonanywhere.com

--- commands/song.js
+++ commands/song.js
@@ -74,7 +74,7 @@ module.exports = {
           .setTitle(`**Beatmap:** ${name}`)
           .setURL(`https://beatsaver.com/beatmap/${key}`)
           .setDescription(
-            `📥 [One click install](http://spectrix.pythonanywhere.com/?key=${key}) with [ModAssistant](https://github.com/Assistant/ModAssistant)!
+            `📥 [One click install](http://spectrix.pythonanywhere.com/botsaber/?key=${key}) with [ModAssistant](https://github.com/Assistant/ModAssistant)!
             👀 [Preview this map in your browser!](https://skystudioapps.com/bs-viewer/?id=${key}) `
           )
           .addFields(
