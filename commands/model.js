0783163c8ba896b79890197697fe4baae5960e60
Spectrix <spectrix@pop-os.localdomain>
2020-10-28 22:24:42 +0200

    Saving work, very incomplete

--- commands/model.js
+++ commands/model.js
@@ -18,5 +18,29 @@ module.exports = {
           "User-Agent": "BeatSaberDiscordBot",
         },
       };
+      const embeds = [];
+
+      for (let i = 1; i <= 5; ++i)
+        embeds.push(new Discord.MessageEmbed().addFields('Page', i)
+        );
+      
+      const myImage = message.author.displayAvatarURL();
+      
+      new Pagination.Embeds()
+        .setArray(embeds)
+        .setAuthorizedUsers([message.author.id])
+        .setChannel(message.channel)
+        .setPageIndicator(true)
+        .setPage(1)
+         // Methods below are for customising all embeds
+        .setTitle('Test Title')
+        .setDescription('Test Description')
+        .setFooter('Test Footer Text')
+        .setURL(myImage)
+        .setColor(0xFF00AE)
+        .addField('\u200b', '\u200b')
+        .addField('Test Field 1', 'Test Field 1', true)
+        .addField('Test Field 2', 'Test Field 2', true)
+        .build();
 
 // incomplete 
