6c71cb666ff0e7c7ad6836a900305ab9bf1d095b
Spectrix <owenhutch123@gmail.com>
2021-02-18 18:20:33 +0200

    Completed

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -1,6 +1,8 @@
 import discord, asyncio, random, time, datetime, json, aiohttp, requests
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
+from disputils import BotEmbedPaginator
+from discord import Embed
 
 with open("config/thesacredtexts.json") as f:
     config = json.load(f)
@@ -14,9 +16,21 @@ class Leaderboard(commands.Cog):
     async def leaderboard(self, ctx):
         """Current scoresaber global leaderboard info"""
         result = requests.get("http://new.scoresaber.com/api/players/1").json()
-        for i in range(10):
-            
-        embed = discord.Embed(title="🏆 **Top 10 Players Leaderboard.**", color=0x309eff, ""
+        data = ''
+        embed = []
+        for i in range (50):
+            player = result['players'][i]
+            data+=(f"`#{player['rank']}.` **{player['playerName']}** | **PP:** {player['pp']} | **Country:** {player['country']} :flag_{player['country'].lower()}:\n")
+            if i == 9 or i == 19 or i == 29 or i == 39 or i == 49:
+                e = Embed(title=f"🏆 Global Leaderboards",
+                      description=data,
+                      color=0x115599)
+                e.set_footer(text="Data fetched from ScoreSaber", icon_url="https://cdn.discordapp.com/attachments/753288806928482354/811991289645760542/rYiHR2pu_400x400.png")
+                embed.append(e)
+                data = ''
+                e = ''
+        paginator = BotEmbedPaginator(ctx, embed)
+        await paginator.run()
 
 def setup(bot):
     bot.add_cog(Leaderboard(bot))
