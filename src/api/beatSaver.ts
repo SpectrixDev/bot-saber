06db3ccfeaf282a48ecce58df4459d48e5ec7380
Samuel Hepworth <samhep0803@gmail.com>
2021-10-23 02:23:02 +0400

    very basic song by id command working

--- src/api/beatSaver.ts
+++ src/api/beatSaver.ts
@@ -1,7 +1,32 @@
 import fetch from "node-fetch";
 
-export const getSongById = async (id: number): Promise<any> => {
-  const response = await fetch(`https://api.beatsaver.com/maps/id/${id}`);
-  const result = await response.json();
-  return result;
+interface SongResult {
+	id: string;
+	name: string;
+	description: string;
+	uploader: {
+		name: string;
+	};
+	metadata: {
+		bpm: number;
+		duration: number;
+	};
+	stats: {
+		downloads: number;
+		upvotes: number;
+		downvotes: number;
+		score: number;
+	};
+	ranked: boolean;
+	versions: Version[];
+}
+
+interface Version {
+	coverURL: string;
+}
+
+export const getSongById = async (id: string): Promise<SongResult> => {
+	const response = await fetch(`https://api.beatsaver.com/maps/id/${id}`);
+	const result: SongResult = await response.json();
+	return result;
 };
--- src/commands/song/Song.ts
+++ src/commands/song/Song.ts
@@ -1,46 +1,58 @@
 import {
-  ApplicationCommandOptionData,
-  ApplicationCommandSubCommandData,
+	ApplicationCommandOptionData,
+	ApplicationCommandSubCommandData,
 } from "discord.js";
 import { getSongById } from "../../api/beatSaver";
 import { Run } from "../../interfaces/Command";
 
 export const run: Run = async (_client, commandInteraction, options) => {
-  const subcommand = options.getSubcommand();
-  if (subcommand === "id") {
-    // const data = getSongById(subcommand.);
-  }
+	const subcommand = options.getSubcommand();
+	let data;
+	if (subcommand === "id") {
+		const value = options.getString("value");
+		if (!value) {
+			return;
+		}
+		data = await getSongById(value);
+
+		const toSend =
+			`Name: ${data.name}\n` +
+			`Author: ${data.uploader.name}\n` +
+			`ID: ${data.id}\n` +
+			`Cover Image: ${data.versions[0].coverURL}\n` +
+			`Description: \`\`\`${data.description}\`\`\``;
+
+		await commandInteraction.reply(toSend);
+	}
 };
 
 export const name: string = "song";
 export const description: string = "Get info on a song/beatmap";
-export const subcommands: ApplicationCommandSubCommandData[] = [
-  {
-    type: "SUB_COMMAND",
-    name: "id",
-    description: "Get song info via its ID.",
-    required: true,
-    options: [
-      {
-        type: "INTEGER",
-        name: "value",
-        description: "Value to get song info from.",
-        required: true,
-      },
-    ],
-  },
-  {
-    type: "SUB_COMMAND",
-    name: "name",
-    description: "Get song info via its name.",
-    required: true,
-    options: [
-      {
-        type: "STRING",
-        name: "value",
-        description: "Value to get song info from.",
-        required: true,
-      },
-    ],
-  },
+export const options: ApplicationCommandOptionData[] = [
+	{
+		type: "SUB_COMMAND",
+		name: "id",
+		description: "Get song info via its ID.",
+		options: [
+			{
+				type: "STRING",
+				name: "value",
+				description: "Value to get song info from.",
+				required: true,
+			},
+		],
+	},
+	{
+		type: "SUB_COMMAND",
+		name: "name",
+		description: "Get song info via its name.",
+		options: [
+			{
+				type: "STRING",
+				name: "value",
+				description: "Value to get song info from.",
+				required: true,
+			},
+		],
+	},
 ];
